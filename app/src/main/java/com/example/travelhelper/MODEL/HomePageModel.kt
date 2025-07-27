ee871740b397538804ec6b5e002e1d87a7c8b995
NikoFan <neoleg2005@gmail.com>
2025-07-27 12:03:48 +0300

    Oleg: Добавил небольшой интерфейс + сделал model и vm для главной страницы; Надо сделать первый JSON для добавления карточек на страницу; Надо селать горизонтальный интерфейс

--- app/src/main/java/com/example/travelhelper/MODEL/HomePageModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.travelhelper.MODEL
-
-class HomePageModel {
-    // Хранение выбранного выбранного топика
-    var selectedTopic: String? = null
-}
---
+++ app/src/main/java/com/example/travelhelper/MODEL/TopicModel.kt
@@ -0,0 +1,9 @@
+package com.example.travelhelper.MODEL
+
+data class Topics (
+    val topicId: Int,
+    val topicTitle: String,
+    val topicImage: Int,
+    val topicMainColor: String,
+    val topicSecondColor: String
+)
