3fe7144a64ab4c83306b2860fb5a18e16172860d
NikoFan <neoleg2005@gmail.com>
2025-07-27 22:59:29 +0300

    Oleg: Добавил JSON + Чтение; Сделал выгрузку иконок и информации из JSON в окно

--- app/src/main/java/com/example/travelhelper/VIEW_MODEL/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/travelhelper/VIEW_MODEL/HomeScreenViewModel.kt
@@ -1,11 +1,54 @@
 package com.example.travelhelper.VIEW_MODEL
 
+import android.content.Context
+import com.google.gson.JsonObject
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.core.graphics.drawable.toBitmap
 import androidx.lifecycle.ViewModel
 import com.example.travelhelper.MODEL.Topics
+import com.example.travelhelper.R
+import com.google.gson.Gson
+import kotlin.reflect.typeOf
 
-class HomeScreenViewModel : ViewModel() {
+class HomeScreenViewModel() : ViewModel() {
+
+    fun ReadJson(
+        context: Context
+    ) {
+        // Прочитка JSON файла в начале работы приложения
+        val jsonString = context.resources.openRawResource(
+            R.raw.topics_json_file
+        )
+            .bufferedReader()
+            .use { it.readText() }
+        val gson = Gson()
+        val jsonObject = gson.fromJson(
+            jsonString,
+            JsonObject::class.java
+        )
+        val themes = jsonObject.getAsJsonObject("Темы")
+
+        println(themes)
+        themes.entrySet().map { entry ->
+            val key = entry.key.toInt()
+            val value = entry.value.asJsonObject
+
+            AddNewTopicInformation(
+                topicInformation = Topics(
+                    topicId = key,
+                    topicTitle = value.get("Название").asString,
+                    topicImage = value.get("Иконка").asString,
+                    topicMainColor = value.get("Основной цвет").asString,
+                    topicSecondColor = value.get("Вторичный цвет").asString,
+                    topicGroup = value.get("Группа").asString
+                )
+            )
+        }
+
+    }
+
     private var listOfTopics = mutableListOf<Topics>()
     private val _topics = mutableStateOf<List<Topics>>(emptyList())
 
