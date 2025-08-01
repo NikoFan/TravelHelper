6053fd3cdeeca8fa2da501e7f949792abfae1489
NikoFan <neoleg2005@gmail.com>
2025-08-01 23:07:27 +0300

    Олег: Добавил карточки и тестовые данные

--- app/src/main/java/com/example/travelhelper/VIEW/AdvicesView.kt
+++ app/src/main/java/com/example/travelhelper/VIEW/AdvicesView.kt
@@ -3,34 +3,25 @@ package com.example.travelhelper.VIEW
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -42,20 +33,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.toColorInt
 import androidx.lifecycle.ViewModelProvider
-import androidx.room.util.TableInfo
-import com.example.travelhelper.MODEL.Topics
 import com.example.travelhelper.VIEW.ui.theme.TravelHelperTheme
 import com.example.travelhelper.VIEW_MODEL.AdviceViewModel
-import com.example.travelhelper.VIEW_MODEL.HomeScreenViewModel
-import kotlin.getValue
 import com.example.travelhelper.MODEL.AdviceModel
+import kotlin.getValue
 
 
 class AdvicesView : ComponentActivity() {
@@ -104,10 +91,35 @@ fun PortraitScreen(
     viewModel: AdviceViewModel
 ) {
     // Состояние для языка с сохранением при повороте
-    var currentLanguage by rememberSaveable { mutableStateOf("ru") }
     val context: Context = LocalContext.current
 
-    Column (
+    // Тестовое добавление данных в карточки
+    viewModel.AddAdviceToList(
+        adviceModel = AdviceModel(
+            adviceIdNumber = 1,
+            adviceTitle = "Название 1",
+            adviceBodyText = "Текст для первого названия",
+            adviceModeDifficulty = "Легко"
+        )
+    )
+    viewModel.AddAdviceToList(
+        adviceModel = AdviceModel(
+            adviceIdNumber = 2,
+            adviceTitle = "Название 2",
+            adviceBodyText = "Текст для первого названия",
+            adviceModeDifficulty = "Средняя"
+        )
+    )
+    viewModel.AddAdviceToList(
+        adviceModel = AdviceModel(
+            adviceIdNumber = 3,
+            adviceTitle = "Название 3 длинное специально для проверки переноса",
+            adviceBodyText = "Текст для первого названия",
+            adviceModeDifficulty = "Сложно"
+        )
+    )
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(8.dp),
@@ -125,31 +137,31 @@ fun PortraitScreen(
                     context = context
                 )
             },
-            ) {Text(text = "Назад")}
+        ) { Text(text = "Назад") }
 
     }
 }
 
 @Composable
 fun LandscapeScreen(viewModel: AdviceViewModel) {
-        // Состояние для языка с сохранением при повороте
-        var currentLanguage by rememberSaveable { mutableStateOf("ru") }
-        val context: Context = LocalContext.current
+    // Состояние для языка с сохранением при повороте
+    var currentLanguage by rememberSaveable { mutableStateOf("ru") }
+    val context: Context = LocalContext.current
 
-        Row (
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Button(
-                onClick = {
-                    GoToMainScreen(
-                        context = context
-                    )
-                }
-            ) { Text(text = "Назад") }
-        }
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(
+            onClick = {
+                GoToMainScreen(
+                    context = context
+                )
+            }
+        ) { Text(text = "Назад") }
+    }
 
     ScrollAreaHorizontal(
         vm = viewModel,
@@ -159,18 +171,43 @@ fun LandscapeScreen(viewModel: AdviceViewModel) {
 
 
 @Composable
-fun ScrollAreaVertical(vm: AdviceViewModel, context: Context) {
+fun ScrollAreaVertical(
+    vm: AdviceViewModel,
+    context: Context
+) {
     //vm.ReadJson(context = context)
-    val topics by vm.topicInformation
+    val advicesList by vm.getAdviceObjects
 
     LazyColumn(
         modifier = Modifier
             .padding(20.dp)
             .fillMaxHeight(0.9f),
     ) {
-        items(topics) { topic ->
-            TopicCardConstructor(
-                topic = topic,
+        items(advicesList) { adviceModelObject ->
+            AdviceCardConstructor(
+                advice = adviceModelObject,
+                cardHeightParam = 150
+            )
+        }
+    }
+}
+
+@Composable
+fun ScrollAreaHorizontal(
+    vm: AdviceViewModel,
+    context: Context
+) {
+    //vm.ReadJson(context = context)
+    val advicesList by vm.getAdviceObjects
+
+    LazyRow(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxHeight(0.8f),
+    ) {
+        items(advicesList) { adviceModelObject ->
+            AdviceCardConstructor(
+                advice = adviceModelObject,
                 cardHeightParam = 200
             )
         }
@@ -178,36 +215,24 @@ fun ScrollAreaVertical(vm: AdviceViewModel, context: Context) {
 }
 
 @Composable
-fun ScrollAreaHorizontal(vm: AdviceViewModel, context: Context) {
-    //vm.ReadJson(context = context)
-    val topics by vm.topicInformation
-
-    LazyRow(
-        modifier = Modifier
-            .padding(20.dp)
-            .fillMaxHeight(0.8f),
-    ) {
-        items(topics) { topic ->
-            TopicCardConstructor(
-                topic = topic,
-                cardHeightParam = 270
-            )
-        }
-    }
-}
-
-@Composable
-fun TopicCardConstructor(
-    topic: Topics,
+fun AdviceCardConstructor(
+    advice: AdviceModel,
     cardHeightParam: Int
 ) {
     val context: Context = LocalContext.current
+
+    val adviceCardBackground = when {
+        advice.adviceModeDifficulty == "Легко" -> Color.Green
+        advice.adviceModeDifficulty == "Средняя" -> Color.Yellow
+        advice.adviceModeDifficulty == "Сложно" -> Color.Red
+        else -> Color.LightGray
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
         colors = CardDefaults.cardColors(
-            containerColor = Color(color = topic.topicSecondColor.toColorInt())
+            containerColor = adviceCardBackground
         ),
         onClick = {
             GoToCard(
@@ -218,30 +243,32 @@ fun TopicCardConstructor(
         Box(
             modifier = Modifier
                 .height(cardHeightParam.dp)
-                .background(color = Color(color = topic.topicMainColor.toColorInt()))
-                .fillMaxWidth(0.8f),
+                .fillMaxWidth(),
         ) {
-            val context = LocalContext.current
-            val resId = context.resources.getIdentifier(
-                topic.topicImage,
-                "drawable",
-                context.packageName
-            )
             Column(
                 modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Bottom
+                verticalArrangement = Arrangement.Center
             ) {
-                Image(
-                    modifier = Modifier.fillMaxSize(0.65f),
-                    painter = painterResource(resId),
-                    contentDescription = "Topic Icon"
-                )
+
                 Text(
-                    modifier = Modifier.padding(10.dp),
-                    text = topic.topicTitle,
-                    fontSize = 35.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(10.dp),
+                    text = advice.adviceTitle,
+                    softWrap = true,
+                    maxLines = 3,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
                     fontWeight = FontWeight.W500
+                )
+
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(horizontal = 10.dp),
+                    text = advice.adviceBodyText,
+                    fontSize = 18.sp
                 )
             }
         }
@@ -249,7 +276,7 @@ fun TopicCardConstructor(
 }
 
 fun GoToMainScreen(
-context: Context
+    context: Context
 ) {
     context.startActivity(
         Intent(
@@ -258,11 +285,3 @@ context: Context
         )
     )
 }
-
-
-data class AdviceModel(
-    val adviceId: Int = 1,
-    val adviceTitle: String = "Название 1",
-    val adviceText: String = "Текст совета",
-    val adviceDifficulty: String = "Сложный"
-)
